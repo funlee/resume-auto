9b70270f879ae01f583451260ed56137eec5e423
funlee <i@funlee.cn>
2017-12-19 01:15:12 +0800

    改为handlebars渲染

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
+var Handlebars = require('handlebars')
+var data = require('../data/resume.json')
+
+var basicHbs = require('../hbs/basic.hbs')
+var contactHbs = require('../hbs/contact.hbs')
+var skillsHbs = require('../hbs/skills.hbs')
+var advantageHbs = require('../hbs/advantage.hbs')
+var workHbs = require('../hbs/work.hbs')
+var projectHbs = require('../hbs/project.hbs')
+
 window.onload = function() {
-  console.log(123)
-  var a = 10
-  var b = 20
-  console.log(a + b)
+  document.querySelector('.basic').innerHTML = basicHbs(data.basic)
+  document.querySelector('.contact').innerHTML = contactHbs(data.contact)
+  document.querySelector('.skills').innerHTML = skillsHbs(data.skills)
+  document.querySelector('.advantage').innerHTML = advantageHbs(data.advantage)
+  document.querySelector('.work').innerHTML = workHbs(data.work)
+  document.querySelector('.project').innerHTML = projectHbs(data.project)
+ 
 }
