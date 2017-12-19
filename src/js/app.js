cb2f94663841d4a665bef9fe07491d163a2e2c7f
funlee <i@funlee.cn>
2017-12-20 01:10:50 +0800

    add loading

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,9 @@
-var Handlebars = require('handlebars')
-var data = require('../data/resume.json')
-
-var basicHbs = require('../hbs/basic.hbs')
-var contactHbs = require('../hbs/contact.hbs')
-var skillsHbs = require('../hbs/skills.hbs')
-var advantageHbs = require('../hbs/advantage.hbs')
-var workHbs = require('../hbs/work.hbs')
-var projectHbs = require('../hbs/project.hbs')
+var render = require('./render.js')
 
 window.onload = function() {
-  document.querySelector('.basic').innerHTML = basicHbs(data.basic)
-  document.querySelector('.contact').innerHTML = contactHbs(data.contact)
-  document.querySelector('.skills').innerHTML = skillsHbs(data.skills)
-  document.querySelector('.advantage').innerHTML = advantageHbs(data.advantage)
-  document.querySelector('.work').innerHTML = workHbs(data.work)
-  document.querySelector('.project').innerHTML = projectHbs(data.project)
+  render()
+  var loading = document.querySelector('.loading')
+  loading.style.opacity = 0
+  loading.style.display = 'none'
  
 }
---
+++ src/js/render.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: funlee 
+ * @Email: i@funlee.cn 
+ * @Date: 2017-12-20 00:10:16 
+ * @Last Modified time:   2017-12-20 00:10:16 
+ * @Description: 组装模版，渲染页面
+ */
+var Handlebars = require('handlebars')
+var data = require('../data/resume.json')
+
+var basicHbs = require('../hbs/basic.hbs')
+var contactHbs = require('../hbs/contact.hbs')
+var skillsHbs = require('../hbs/skills.hbs')
+var advantageHbs = require('../hbs/advantage.hbs')
+var workHbs = require('../hbs/work.hbs')
+var projectHbs = require('../hbs/project.hbs')
+
+var render = function() {
+  document.querySelector('.basic').innerHTML = basicHbs(data.basic)
+  document.querySelector('.contact').innerHTML = contactHbs(data.contact)
+  document.querySelector('.skills').innerHTML = skillsHbs(data.skills)
+  document.querySelector('.advantage').innerHTML = advantageHbs(data.advantage)
+  document.querySelector('.work').innerHTML = workHbs(data.work)
+  document.querySelector('.project').innerHTML = projectHbs(data.project)
+  
+}
+
+module.exports = render
